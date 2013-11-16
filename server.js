76c5174e41f2372ce9744292d19d45c0a6116848
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-11-17 00:10:17 +0900

    Done!

--- server.js
+++ server.js
@@ -3,20 +3,10 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " recieved.");
 
-    request.setEncoding("utf8");
-
-    request.addListener("data", function(chunk) {
-      postData += chunk;
-      console.log("Received POST data chunk '" + chunk + " '.");
-    });
-
-    request.addListener("end", function() {
-      route(handle, pathname, response, postData);
-    });
+    route(handle, pathname, response, request);
 
   }
 
